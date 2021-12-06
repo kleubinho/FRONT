4fea025135d27b7870dd6b407df54cb1c8d0d52f
KLEBER HENRIQUE FELIX DOS SANTOS <kleber.hfsantos@senacsp.edu.br>
2021-12-06 12:00:01 -0300

    first commit

---
+++ css/Style.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        height: "100%",
+        width: "100%",
+        padding: 10
+
+    },
+
+    imgcont: {
+        marginTop: 80,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+
+    img: {
+        height: 140,
+        width: 240,
+
+    },
+
+    controll: {
+        width: "80%",
+        justifyContent:"center",
+        
+        
+    },
+
+    input: {
+        marginTop:20,
+        borderColor: "#eee",
+        borderWidth: 1.5,
+        borderRadius:10,
+        padding: 20,
+        marginBottom: 10,
+        fontSize: 17,
+        fontWeight:"600",
+        
+
+    }
+})
---
+++ screens/Home.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react-native';
+
+export default function Home() {
+
+}
